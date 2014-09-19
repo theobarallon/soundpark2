9ed3cc474b9d18241896771f07c06d07df30fb0f
ArthurRougier <arthur.rougier@gmail.com>
2014-09-19 17:41:38 +0200

    Changed curator style into curator genre

--- model/get_song_boxes.php
+++ model/get_song_boxes.php
@@ -1,3 +1,3 @@
 <?php
 include_once('connect_sql.php');
-$req = $bdd->query('SELECT artwork_url, artist, title, genre, pseudo FROM song, playlist, curator WHERE song.ID_playlist=playlist.ID AND playlist.date_end >= NOW() AND playlist.date_start <= NOW() AND song.ID_curator = curator.ID ORDER BY song.id');
+$req = $bdd->query('SELECT artwork_url, artist, title, song.genre, pseudo FROM song, playlist, curator WHERE song.ID_playlist=playlist.ID AND playlist.date_end >= NOW() AND playlist.date_start <= NOW() AND song.ID_curator = curator.ID ORDER BY song.id');
