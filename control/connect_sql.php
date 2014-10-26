f9a9ed84c3ad23f05d17d9142803813868ac7f49
ArthurRougier <arthur.rougier@gmail.com>
2014-10-26 10:55:37 +0100

    SQL

--- control/connect_sql.php
+++ control/connect_sql.php
@@ -2,7 +2,7 @@
 	session_start();
 	try
 	{
-		$bdd = new PDO('mysql:host=localhost;dbname=soundpark2', 'root', 'mWLbiJWD73');
+		$bdd = new PDO('mysql:host=localhost;dbname=soundpark2', 'root', 'root');
 	}
 	catch (Exception $e)
 	{
