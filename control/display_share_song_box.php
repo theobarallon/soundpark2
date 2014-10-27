bbab153a2e7838bbadb5505f425b338d2a2636d7
ArthurRougier <arthur.rougier@gmail.com>
2014-10-27 22:24:52 +0100

    Fromshare likes bug fix

--- control/display_share_song_box.php
+++ control/display_share_song_box.php
@@ -3,7 +3,7 @@ include_once('../model/get_share_song_box.php');
 ?>
 <div id="sound_box<?php echo($i);?>" class="sound_box">
 	<img src="<?php echo($songBox[0]);?>" class="sound_cover"></img>
-	<div class="ribbon"><?php echo(round($songBoxes[5]*7,26)); ?> likes cette semaine</div>
+	<div class="ribbon"><?php echo(round($songBox[5]*7,26)); ?> likes cette semaine</div>
 	<div class="cover_wrapper" id="cover_wrapper<?php echo($i);?>"><input type="button" class="play" id="play" value="play"/></div>
 	<div class="sound_informations">
 		<h3> Artiste : <?php echo($songBox[1]);?> </br></h3>
